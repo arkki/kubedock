da9abf4d248abac5e1d4a065c8490f873bdf0fff
arkki <arkkidump@gmail.com>
2025-08-27 11:38:25 +0300

    compile scripts

---
+++ scripts/compile-linux-arm64.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+export KUBEDOCK_VERSION=0.18.1-1-custom-lifetime
+
+rm -rf dist
+
+go mod download
+LDFLAGS="-s -w -X main.version=${KUBEDOCK_VERSION}"
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build \
+  -trimpath -buildvcs=false -ldflags="${LDFLAGS}" \
+  -o dist/kubedock .
+
+mkdir -p dist/pkg
+cp dist/kubedock dist/pkg/
+cp LICENSE dist/pkg/
+tar -C dist/pkg -czf "dist/kubedock_${KUBEDOCK_VERSION}_linux_arm64.tar.gz" .
+shasum -a 256 "dist/kubedock_${KUBEDOCK_VERSION}_linux_arm64.tar.gz" > "dist/kubedock_${KUBEDOCK_VERSION}_linux_arm64.tar.gz.sha256"
